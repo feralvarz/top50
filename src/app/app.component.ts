f9dcd00aefbb0bcb3b4751833253311b10933277
Fernando Alvarez <fernando.alvarez@centricdigital.com>
2019-05-12 14:16:29 -0300

    Preparing mockdata to test card repeater

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+declare const require;
+
+const mockData = require('./mockdata.json');
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  constructor() {
+    console.log(mockData);
+  }
 }
