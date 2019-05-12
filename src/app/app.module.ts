9accd49c982ac70d49eacc58dc5dc3afca322328
Fernando Alvarez <fernando.alvarez@centricdigital.com>
2019-05-12 14:38:49 -0300

    Adding Detail Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
+import { DetailComponent } from './detail/detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
+    CardComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule
